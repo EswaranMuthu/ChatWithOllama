c7e6a991306b8d6afa00568870e06891d7a4e8f7
Eswaran Muthu <38020741+EswaranMuthu@users.noreply.github.com>
2024-12-21 11:03:59 -0500

    RAG with hugging face documents.

---
+++ RAGwithhuggingface.py
@@ -0,0 +1,61 @@
+from haystack_integrations.components.generators.ollama import OllamaGenerator
+from haystack import Pipeline, Document
+from datasets import load_dataset
+from haystack.components.retrievers.in_memory import InMemoryBM25Retriever
+from haystack.components.builders.prompt_builder import PromptBuilder
+from haystack.document_stores.in_memory import InMemoryDocumentStore
+import gradio as gr
+
+template = """
+Given the following information, answer the question.
+
+Context: 
+{% for document in documents %}
+    {{ document.content }}
+{% endfor %}
+
+Question: {{ query }}?
+"""
+
+dataset = load_dataset("bilgeyucel/seven-wonders", split="train")
+docs = [Document(content=doc["content"], meta=doc["meta"]) for doc in dataset]
+
+#print(docs)
+
+document_store = InMemoryDocumentStore()
+document_store.write_documents(docs)
+
+retriever = InMemoryBM25Retriever(document_store=document_store)
+#print(retriever.run(query="Is Tomb of Mausolus is part of senven wonder?"))
+
+generator = OllamaGenerator(model="zephyr",
+                            url = "http://localhost:11434",
+                            timeout=12000,
+                            generation_kwargs={
+                              "num_predict": 100,
+                              "temperature": 0.9,
+                              })
+
+pipe = Pipeline()
+pipe.add_component("retriever", retriever)
+pipe.add_component("prompt_builder", PromptBuilder(template=template))
+pipe.add_component("llm", generator)
+pipe.connect("retriever", "prompt_builder.documents")
+pipe.connect("prompt_builder", "llm")
+
+def ask_question(question):
+  result = pipe.run({"prompt_builder": {"query": question},
+									"retriever": {"query": question}})
+  return result["llm"]["replies"][0]
+
+gr_interface = gr.Interface(
+  fn=ask_question,
+  inputs=gr.components.Textbox(lines=2, placeholder="Enter your question here..."),
+  outputs="text"
+)
+
+gr_interface.launch()
+# {'llm': {'replies': ['Based on the provided context, it seems that you enjoy
+# soccer and summer. Unfortunately, there is no direct information given about 
+# what else you enjoy...'],
+# 'meta': [{'model': 'zephyr', ...]}}
